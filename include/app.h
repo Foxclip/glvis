789a9d3eede12117653917108d5f90780149ad87
Foxclip <destroyApp2@gmail.com>
2025-12-02 16:18:53 +1000

    Create App class

---
+++ include/app.h
@@ -0,0 +1,18 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+class App {
+
+public:
+    App();
+    ~App();
+
+private:
+    GLFWwindow* window = nullptr;
+
+    GLFWwindow* init();
+    void mainLoop();
+
+};
